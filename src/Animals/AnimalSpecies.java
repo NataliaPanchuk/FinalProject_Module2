c74134b2c6e56d9e4f8454d3e3cd32f45a1a00ac
NataliaPanchuk <Panchuk_n_y@ukr.net>
2024-04-13 17:37:54 +0300

    Add ENUM

---
+++ src/Animals/AnimalSpecies.java
@@ -0,0 +1,22 @@
+package Animals;
+
+public enum AnimalSpecies {
+
+    BOAR,
+    BUFFALO,
+    CATERPILLAR,
+    DEER,
+    DUCK,
+    GOAT,
+    HORSE,
+    MOUSE,
+    RABBIT,
+    SHEEP,
+    BEAR,
+    BOA,
+    EAGLE,
+    FOX,
+    WOLF,
+    PLANTS;
+
+}
